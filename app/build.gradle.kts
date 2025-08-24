9fc1490dada004305da707fd2aafcc4e8d7e177e
Bulka3100 <kirillvip3100@yandex.ru>
2025-08-24 06:57:17 +0300

    M-02

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,12 +15,12 @@ android {
         }
     }
     namespace = "com.example.mirmessager"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.mirmessager"
         minSdk = 24
-        targetSdk = 34
+        targetSdk = 35
         versionCode = 1
         versionName = "1.0"
 
